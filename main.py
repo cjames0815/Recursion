1d7812caba7a6d999210565401cf8e375028cba2
Cameron James <cjames0815@gmail.com>
2023-12-06 10:38:46 -0500

    First Version of reverse

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ print("Factorial of 6 using a loop is:", loops.factorial(6))
 print("Factorial of 5 using a loop is:", loops.factorial(5))
 print("Factorial of 1 using a loop is:", loops.factorial(1))
 
-print("Factorial of 6 using a recursion is:", loops.factorial(6))
-print("Factorial of 5 using a recursion is:", loops.factorial(5))
-print("Factorial of 1 using a recursion is:", loops.factorial(1))
+print("Factorial of 6 using a recursion is:", recursion.factorial(6))
+print("Factorial of 5 using a recursion is:", recursion.factorial(5))
+print("Factorial of 1 using a recursion is:", recursion.factorial(1))
 
 print("2 to the 5th power using a loop is:", loops.power(2,5))
 print("2 to the 4th power using a loop is:", loops.power(2,4))
@@ -17,3 +17,10 @@ print("2 to the 5th power using a recursion is:", recursion.power(2,5))
 print("2 to the 4th power using a recursion is:", recursion.power(2,4))
 print("2 to the 0 power using a recursion is:", recursion.power(2,0))
 
+nums = [5,12,3,4]
+print("Mininum number using a loop is", loops.computeMin(nums))
+print("Mininum number using a recursion is", recursion.computeMin(nums,0, nums[0]))
+
+str= "CAT"
+loops.reverse(str)
+recursion.reverse(str, len(str))
